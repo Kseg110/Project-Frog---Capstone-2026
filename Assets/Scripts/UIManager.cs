b3e2429b8fe2c4ca302fff77439742c3102d4527
Elkentor <pat_ginch@hotmail.com>
2026-01-10 20:57:15 -0500

    Add main menu scene and initial assets
    Added the Main Menu Unity scene, including UI elements and background image. Introduced initial scripts for fullscreen, game management, language support, and UI management. Imported TextMesh Pro resources and a project image asset.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Sub-menus")]
+    public GameObject settingsSubMenu;  // Contain Audio, Video, Language, Controls
+
+    [Header("Primary Buttons")]
+    public GameObject startButton;
+    public GameObject optionButton;
+    public GameObject exitButton;
+    public GameObject creditsButton;
+
+    private bool isStartExpanded = false;
+    private bool isSettingsExpanded = false;
+
+    void Start()
+    {
+        settingsSubMenu.SetActive(false);
+    }
+
+    // --- call primary fonction ---
+    public void OnStartClicked()
+    {
+       
+    }
+
+    public void OnSettingsClicked()
+    {
+       
+    }
+
+    public void OnExitClicked()
+    {
+        Application.Quit();
+    }
+
+    // --- call sub menu ---
+ 
+    public void OnAudioClicked()
+    {
+        Debug.Log("Menu Audio open");
+        // show audio options
+    }
+
+    public void OnVideoClicked()
+    {
+        Debug.Log("Menu Video open");
+        // show video options
+    }
+
+    public void OnLanguageClicked()
+    {
+        Debug.Log("Menu Language open");
+        // show language options
+    }
+
+    public void OnControlsClicked()
+    {
+        Debug.Log("Menu Control open");
+        // show controls options
+    }
+}
