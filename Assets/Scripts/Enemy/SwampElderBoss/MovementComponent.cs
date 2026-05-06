52d2b58f1a3f7074e4f3221657af13bf7241288a
AlpiBoi <adam.desro754@gmail.com>
2026-05-06 12:57:25 -0400

    added boss state machine
    includes combat and movement component

---
+++ Assets/Scripts/Enemy/SwampElderBoss/MovementComponent.cs
@@ -0,0 +1,92 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MovementComponent : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    [Header("Target")]
+    [SerializeField] private Transform target;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float stopDistance = 3f;
+    [SerializeField] private float movementSpeed = 1f;
+    [SerializeField] private float repathRate = 0.2f;  //how often the enemie's path is recalculated//
+
+    private float repathTimer;
+    private bool canMove = true;
+
+    private void Awake()
+    { 
+        agent = GetComponent<NavMeshAgent>(); 
+    }
+
+    private void Start()
+    {
+        agent.stoppingDistance = stopDistance;
+        agent.speed = movementSpeed;
+    }
+
+    private void Update()
+    {
+        if (!canMove || target == null) return;
+
+        repathRate -= Time.deltaTime;
+
+        if (repathTimer <= 0f)
+        {
+            MoveTo(target.position);
+            repathTimer = repathRate;
+        }
+    }
+
+
+    //change target//
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+
+    public void MoveTo(Vector3 position)
+    {
+        if(!agent.enabled) return;
+
+        agent.isStopped = false;
+        agent.SetDestination(position);
+    }
+
+    public void Stop()
+    {
+        if (!agent.enabled) return;
+
+        agent.isStopped = true;
+        agent.ResetPath();
+    }
+
+    public bool HasReachedDestination()
+    {
+        if(!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public float GetDistanceToTarget()
+    {
+        if(target == null) return Mathf.Infinity;
+        return Vector3.Distance(transform.position,target.position);
+    }
+
+    public void SetMovementEnabled(bool enabled)
+    {
+        canMove = enabled;
+
+        if (!enabled)
+            Stop();
+    }
+
+}
