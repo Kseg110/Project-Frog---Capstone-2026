dd5f889967f07c8c311178706a52b1287a2ea2eb
Elkentor <pat_ginch@hotmail.com>
2026-08-16 20:20:15 -0400

    MainMenu Revamp
    Change the system for the main menu  to be revert back if those change are not wanted.  1 Last thing that need to be fix is that the default button should only preselected for when the pannel open after that it should keep the last one Over as the selected one

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,60 +5,60 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [Header("Sub-Menus Options")]
-    [SerializeField] private Button audioButton;
-    [SerializeField] private Button videoButton;
-    [SerializeField] private Button controlsButton;
+    [Header("Panels")]
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject audioPanel;
+    [SerializeField] private GameObject videoPanel;
+    [SerializeField] private GameObject controlsPanel;
 
-    [Header("Primary Buttons")]
-    [SerializeField] private Button startButton;
-    [SerializeField] private Button optionButton;
-    [SerializeField] private Button exitButton;
-    [SerializeField] private Button creditsButton;
-
-    [Header("Sub-Menus Audio")]
-    [SerializeField] private Slider sfxSlider;
-    [SerializeField] private TextMeshProUGUI sfxLabel;
-
-    [SerializeField] private Slider musicSlider;
-    [SerializeField] private TextMeshProUGUI musicLabel;
-
-    [SerializeField] private Slider masterSlider;
-    [SerializeField] private TextMeshProUGUI masterLabel;
-
-    [Header("Sub-Menus Video")]
-
-
-    [Header("Sub-Menus Controls")]
-    [SerializeField] private GameObject keyboardImage;
-    [SerializeField] private GameObject controllerImage;
-
-
-    private bool isOptionsExpanded;
-    private bool isAudioButtonOpen;
-    private bool isVideoButtonOpen;
-    private bool isControlsButtonOpen;
+    private GameObject currentPanel;
 
     private void Start()
     {
-        audioButton.gameObject.SetActive(false);
-        videoButton.gameObject.SetActive(false);
-        controlsButton.gameObject.SetActive(false);
-
-        sfxSlider.gameObject.SetActive(false);
-        sfxLabel.gameObject.SetActive(false);
-
-        musicSlider.gameObject.SetActive(false);
-        musicLabel.gameObject.SetActive(false);
-
-        masterSlider.gameObject.SetActive(false);
-        masterLabel.gameObject.SetActive(false);
-
-        keyboardImage.SetActive(false);
-        controllerImage.SetActive(false);
+        ShowPanel(mainMenuPanel);
     }
 
-    // --- call primary fonction ---
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Cancel"))
+            {
+                OnBackClicked();
+            }
+        }
+    }
+
+    // ---------------- PANEL MANAGEMENT ----------------
+
+    public void ShowPanel(GameObject panel)
+    {
+        if (currentPanel != null)
+            currentPanel.SetActive(false);
+
+        currentPanel = panel;
+        currentPanel.SetActive(true);
+    }
+
+    public void OnBackClicked()
+    {
+        if (currentPanel == audioPanel || currentPanel == videoPanel || currentPanel == controlsPanel)
+        {
+            ShowPanel(optionsPanel);
+        }
+        else if (currentPanel == optionsPanel)
+        {
+            ShowPanel(mainMenuPanel);
+        }
+        else
+        {
+            ShowPanel(mainMenuPanel);
+        }
+    }
+
+    // ---------------- PRIMARY BUTTONS ----------------
+
     public void OnStartClicked()
     {
         SceneManager.LoadScene("GameScene");
@@ -66,42 +66,14 @@ public class UIManager : MonoBehaviour
 
     public void OnOptionsClicked()
     {
-        // open/close settings menu
-        isOptionsExpanded = !isOptionsExpanded;
-        
-        audioButton.gameObject.SetActive(isOptionsExpanded);
-        videoButton.gameObject.SetActive(isOptionsExpanded);
-        controlsButton.gameObject.SetActive(isOptionsExpanded);
-
-        // Close audio sub-menu if options menu is closed
-        if (!isOptionsExpanded)
-        {
-            isAudioButtonOpen = false;
-
-            sfxSlider.gameObject.SetActive(false);
-            sfxLabel.gameObject.SetActive(false);
-
-            musicSlider.gameObject.SetActive(false);
-            musicLabel.gameObject.SetActive(false);
-
-            masterSlider.gameObject.SetActive(false);
-            masterLabel.gameObject.SetActive(false);
-        }
-
-        // Close controls sub-menu if options menu is closed
-        if (!isOptionsExpanded)
-        {
-            isControlsButtonOpen = false;
-            keyboardImage.SetActive(false);
-            controllerImage.SetActive(false);
-        }
+        ShowPanel(optionsPanel);
     }
 
     public void OnExitClicked()
     {
         Application.Quit();
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false; // Stops play mode in the editor
+        UnityEditor.EditorApplication.isPlaying = false;
 #endif
     }
 
@@ -110,77 +82,20 @@ public class UIManager : MonoBehaviour
         SceneManager.LoadScene("Credits");
     }
 
-    //Sub-menu functions
-
-    public void OnVideoClicked()
-    {
-        if (!isVideoButtonOpen)
-            CloseAllSubMenus();
-
-        isVideoButtonOpen = !isVideoButtonOpen;
-
-        Debug.Log("Menu Video open");
-        //Disable the Video button if audio menu is open
-        videoButton.GetComponent<UnityEngine.UI.Button>().interactable = !isVideoButtonOpen;
-    }
+    // ---------------- SUB-MENUS ----------------
 
     public void OnAudioClicked()
     {
-        if (!isAudioButtonOpen)
-            CloseAllSubMenus();
+        ShowPanel(audioPanel);
+    }
 
-        // open/close audio options
-        isAudioButtonOpen = !isAudioButtonOpen;
-
-        masterSlider.gameObject.SetActive(isAudioButtonOpen);
-        masterLabel.gameObject.SetActive(isAudioButtonOpen);
-
-        sfxSlider.gameObject.SetActive(isAudioButtonOpen);
-        sfxLabel.gameObject.SetActive(isAudioButtonOpen);
-
-        musicSlider.gameObject.SetActive(isAudioButtonOpen);
-        musicLabel.gameObject.SetActive(isAudioButtonOpen);
-
-        //Disable the Audio button if audio menu is open
-        audioButton.GetComponent<UnityEngine.UI.Button>().interactable = !isAudioButtonOpen;
+    public void OnVideoClicked()
+    {
+        ShowPanel(videoPanel);
     }
 
     public void OnControlsClicked()
     {
-        if (!isControlsButtonOpen)
-            CloseAllSubMenus();
-
-        isControlsButtonOpen = !isControlsButtonOpen;
-
-        keyboardImage.SetActive(isControlsButtonOpen);
-        controllerImage.SetActive(isControlsButtonOpen);
-
-        //Disable the Controls button if audio menu is open
-        controlsButton.GetComponent<UnityEngine.UI.Button>().interactable = !isControlsButtonOpen;
+        ShowPanel(controlsPanel);
     }
-
-    private void CloseAllSubMenus()
-    {
-        // Audio
-        isAudioButtonOpen = false;
-        masterSlider.gameObject.SetActive(false);
-        masterLabel.gameObject.SetActive(false);
-        sfxSlider.gameObject.SetActive(false);
-        sfxLabel.gameObject.SetActive(false);
-        musicSlider.gameObject.SetActive(false);
-        musicLabel.gameObject.SetActive(false);
-
-        audioButton.interactable = true;
-        videoButton.interactable = true;
-        controlsButton.interactable = true;
-
-        // Controls
-        isControlsButtonOpen = false;
-        keyboardImage.SetActive(false);
-        controllerImage.SetActive(false);
-
-        // Video
-        isVideoButtonOpen = false;
-    }
-
 }
