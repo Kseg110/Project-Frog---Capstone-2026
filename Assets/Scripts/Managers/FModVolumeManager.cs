3baa992198c2dd93847799e92413bb1caba8f9be
e-wj-m <eric.milroy@hotmail.ca>
2026-09-10 04:44:34 -0400

    Final UI Elements Update - FMod Sliders Main Menu -> Game Scene Fix
    This update contains all of the final UI sprites updated and implemented in the form of prefabs for the Main Menu, Pause Menu and generalized UI elements (Restart/Main Menu button on Death Screen, etc). I've also gone ahead and updated the FModVolumeManager.cs script, which allows us to finally connect the Main Menu's FMod audio sliders to the Pause Menu's FMod audio sliders in the Pause Menu within GameScene - however, there is some additional setup required that I am unable to do right now without causing game scene changes, so we'll need to reconvene on the weekend to hook them up.
    
    However, for the time being, UI Setup is as follows;
    
    MAIN MENU CHANGES
    I've created a prefab that handles the entirety of the main menu's original canvas, named 'UpdatedMainMenuCanvas'. While this does hold ALL Canvas updates for the Main Menu, including the Audio options, the unfortunate side effect of replacing the basic 'Canvas' object this prefab is based off of means all of the OnClick references for each button will need to be re-assigned in order to make sure that it works. However, once re-assigned, all works as intended. NOTE: I've also disabled the 'Video' and 'Controls' buttons in this new prefab for the time being, until we can update those menus properly.
    
    GAME SCENE UI CHANGES
    Firstly, the Tutorial Popups object has also been turned into it's own prefab; 'TutorialPopups' (so complex, I know). This handles replacing all the old Tutorial Messages with the new UI images. This can simply replace the old TutorialPopups object, but in order to work properly, the new Prefab parent object will need to have it's transform data set to 0, 0, 0 in order for the triggers to be in the right places - doing this will ensure everything works as intended once again with the new images.
    
    Secondly, I've updated the Pause Menu to hold some new Audio Sliders that I've created into their own prefabs; 'MasterSlider', 'SFXSlider' and 'MusicSlider'. These are the same ones copied over from the 'UpdatedMainMenuCanvas', just turned into their own respective prefabs to be used in the pause menu separately. I've also ensured that the buttons have been updated in the pause menu, as well as in the Death Menu.
    
    Finally, the Health Bar and Fly UI elements have been updated as well, but there have been some odd issues with the changes I've overrided to the UIPlayerHUD prefab not applying for the HealthBackground object. Everything else should be in place, but for the sake of being consistent, I'll list the transform values for the HealthBackground and HealthFill object and what they SHOULD be at below;
    
    HEALTH BACKGROUND
    - Anchored at Center Top
    - Pos. X = 0
    - Pos. Y = -51.5
    - Width = 600
    - Height = 100
    
    HEALTH FILL
    - Anchored at Center Top
    - Pos. X = 0
    - Pos. Y = -27.5
    - Width = 408.5
    - Height = 16.5
    
    FMOD SLIDERS CARRY OVER FIX (Main Menu -> Game Scene)
    Lastly, I've updated the FModVolumeManager.cs script to allow any changes to the audio sliders in the Main Menu to carry over to the Game Scene, and they can also have their audio levels changed in Game Scene as well. Each Slider will need to be assigned to the corresponding field on an object that holds the FModVolumeManager script, in both the MainMenu and GameScene scenes, as well as ensuring that each slider is set to have a 'SetMasterVolume' event on the slider (or Music/SFX).
    
    MISC. CHANGES
    I did go ahead and import the Quatl font back into the editor as well, and created a new font asset in order to be used in future easily. I believe all other text is fine for now!
    
    That concludes my changes to this branch, I'll make some additional FMod updates to try and balance our audio a bit more over the weekend as we assemble the build as well as some general changes to help speed up the build assembly. So close!! -E.M

--- Assets/Scripts/Managers/FModVolumeManager.cs
+++ Assets/Scripts/Managers/FModVolumeManager.cs
@@ -2,22 +2,51 @@ using FMOD;
 using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FMODTest : MonoBehaviour
 {
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider sfxSlider;
+    [SerializeField] private Slider musicSlider;
+
     private Bus master;
+    private Bus sfx;
+    private Bus music;
 
     void Start()
     {
         master = RuntimeManager.GetBus("bus:/");
+        sfx = RuntimeManager.GetBus("bus:/");
+        music = RuntimeManager.GetBus("bus:/");
 
-        //RESULT result = master.setVolume(0.5f);
+        SyncSlider(masterSlider, master);
+        SyncSlider(sfxSlider, sfx);
+        SyncSlider(musicSlider, music);
+    }
 
-        //UnityEngine.Debug.Log(result);
+    private void SyncSlider(Slider slider, Bus bus)
+    {
+        if (slider == null || !bus.isValid()) return;
+
+        if (bus.getVolume(out float volume) == RESULT.OK)
+        {
+            slider.SetValueWithoutNotify(volume);
+        }
     }
 
     public void SetMasterVolume(float value)
     {
         master.setVolume(value);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        sfx.setVolume(value);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        music.setVolume(value);
     }
 }
